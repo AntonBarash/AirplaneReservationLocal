7fbf3a937c10555d5ce158b92df1e0d57ece30d2
henryliu6240 <95504577+henryliu6240@users.noreply.github.com>
2021-12-06 21:32:06 -0500

    Update Admin.java

--- src/main/java/Admin.java
+++ src/main/java/Admin.java
@@ -1,1 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class Admin {
+	private String email;
+	private String pass;
+	private String userType;
+	private boolean loggedIn;
+	
+	public User() {
+		this.loggedIn = false;
+		this.email = null;
+		this.pass = null;
+		this.userType = null;
+	}
+	
+	public User(String email, String pas) {
+		this.loggedIn = true;
+		this.email = email;
+		this.pass = pas;
+		this.userType = "admin";
+	}
+	
+	public String getUserType() {
+		return userType;
+	}
+	
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+}
